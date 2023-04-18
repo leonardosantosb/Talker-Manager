815edd63dcbd20abcdf2844471fee1ae642b3709
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-04-17 22:22:37 -0300

    req 4

--- src/index.js
+++ src/index.js
@@ -38,10 +38,53 @@ app.get('/talker/:id', async(req, res) => {
   return res.status(404).json({"message": "Pessoa palestrante não encontrada"})
 })
 
-app.post('/login', async(req, res) => {
+const emailValidation = (req, res, next) => {
+  const {email} = req.body;
+  // const isValid = Object.values(talks).every((value) => value.length === 0);
+  if(!email){
+    return res.status(400).json({
+      "message": "O campo \"email\" é obrigatório"
+    })
+  }
+  next()
+}
+const emailFormat = (req, res, next) => {
+  const {email} = req.body;
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const RegexAprov = regex.test(email)
+  if(!RegexAprov){
+    return res.status(400).json({
+      "message": "O \"email\" deve ter o formato \"email@email.com\""
+    })
+  }
+  next()
+}
+
+const passwordValidation = (req, res, next) => {
+  const {password} = req.body;
+  if(!password){
+    return res.status(400).json({
+      "message": "O campo \"password\" é obrigatório"
+    })
+  }
+  next()
+}
+
+const passwordLength = (req, res, next) => {
+  const {password} = req.body;
+  if(password.length < 6){
+    return res.status(400).json({
+      "message": "O \"password\" deve ter pelo menos 6 caracteres"
+    })
+  }
+  next()
+}
+
+app.post('/login', emailValidation, emailFormat, passwordValidation, passwordLength, async(req, res) => {
   console.log(req);
   const token = Math.floor(Math.random() * 10000000000000000).toString();
 
   return res.status(200).json({"token":token});
   
 })
+
